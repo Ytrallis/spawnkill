27345238052259e72b092af0568f1aee7e681ff6
dorian-marchal <marchal.dorian@gmail.com>
2016-01-31 21:52:17 +0100

    Re-enable InfosPseudo module

--- jvc-spawnkill.user.js
+++ jvc-spawnkill.user.js
@@ -25,7 +25,7 @@
 // @require     modules/Settings.js?v3
 // @require     modules/Quote.js?v3
 // @require     modules/Shortcuts.js?v3
-// @disabled     modules/InfosPseudo.js?v3
+// @require     modules/InfosPseudo.js?v3
 // @require     modules/HilightSpecialTopic.js?v3
 // @require     modules/LastPage.js?v3
 // @require     modules/EmbedMedia.js?v3
