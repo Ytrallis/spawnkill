0fffb56ce308bb76f91440097a79688d326e6fe0
dorian-marchal <marchal.dorian@gmail.com>
2016-02-06 19:26:38 +0100

    Improve test readability

--- modules/InfosPseudo.js
+++ modules/InfosPseudo.js
@@ -175,7 +175,10 @@ SK.moduleConstructors.InfosPseudo.prototype.addPostButtons = function(message) {
     var topicSearchUrl = location.protocol + "//www.jeuxvideo.com/recherche" + forumUrl + "?type_search_in_forum=auteur_topic&search_in_forum=" + message.authorPseudo;
 
     // Bouton profil
-    if ((this.getSetting("enableProfile") || (this.getSetting("enableSex") && message.author.gender[0] != "u")) && !message.author.profileUnavailable) {
+    if (!message.author.profileUnavailable && (
+        this.getSetting("enableProfile") ||
+        (this.getSetting("enableSex") && message.author.gender !== "unknown")
+    )) {
 
         var profileButtonOptions = {
             class: (message.author.gender && this.getSetting("enableSex")) ? message.author.gender : "unknown",
